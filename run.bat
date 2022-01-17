be49f186c8d258556c11a980d3f14fb00a880f99
Lienkaiyu <37901261+Lienkaiyu@users.noreply.github.com>
2022-01-18 01:20:21 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,9 +2,13 @@ if not exist %USERPROFILE%\Documents\WindowsPowerShell\ (mkdir %USERPROFILE%\Doc
 echo $ProgressPreference = 'SilentlyContinue' > %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 Powershell.exe Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 echo downloading Git
+if not exist Git.exe (
 Powershell.exe Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-64-bit.exe -O Git.exe
-Git.exe /SILENT
-Powershell.exe Invoke-WebRequest https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1-windows-x86_64.msi -O CMake.msi
+)
+  Git.exe /SILENT
+if not exist CMake.msi (
+  Powershell.exe Invoke-WebRequest https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1-windows-x86_64.msi -O CMake.msi
+)
 CMake.msi /passive
 set PATH=%PATH%;C:\Program Files\Git;C:\Program Files\CMake\bin
 Powershell.exe Invoke-WebRequest https://github.com/Lienkaiyu/quick-install-darknet/vs_professional.exe
