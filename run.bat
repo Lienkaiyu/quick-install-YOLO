1258de482dd00ed5a91290a71ce281fba12b5534
Lienkaiyu <37901261+Lienkaiyu@users.noreply.github.com>
2022-01-18 01:28:06 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,21 +3,35 @@ echo $ProgressPreference = 'SilentlyContinue' > %USERPROFILE%\Documents\WindowsP
 Powershell.exe Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 echo downloading Git
 if not exist Git.exe (
-Powershell.exe Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-64-bit.exe -O Git.exe
+  Powershell.exe Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-64-bit.exe -O Git.exe
 )
-  Git.exe /SILENT
+Git.exe /SILENT
+
+echo downloading CMake
 if not exist CMake.msi (
   Powershell.exe Invoke-WebRequest https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1-windows-x86_64.msi -O CMake.msi
 )
 CMake.msi /passive
-set PATH=%PATH%;C:\Program Files\Git;C:\Program Files\CMake\bin
-Powershell.exe Invoke-WebRequest https://github.com/Lienkaiyu/quick-install-darknet/blob/main/vs_Professional.exe
+
+echo downloading Visual Studio 2017 Installer
+if not exist vs_Professional.exe(
+  Powershell.exe Invoke-WebRequest https://github.com/Lienkaiyu/quick-install-darknet/blob/main/vs_Professional.exe
+)
 vs_professional.exe --addProductLang en-us
-"C:\Program Files\Git\bin\sh.exe" --login -i -c "mkdir /c/lib"
-COPY installOCV.sh C:\lib
+
+set PATH=%PATH%;C:\Program Files\Git;C:\Program Files\CMake\bin
+
+mkdir c:\lib
 cd c:\lib
+if not exist installOCV.sh(
+  Powershell.exe Invoke-WebRequest https://github.com/Lienkaiyu/quick-install-darknet/blob/main/installOCV.sh
+)
 "C:\Program Files\Git\bin\sh.exe" --login -i -c "./installOCV.sh"
-git clone https://github.com/AlexeyAB/darknet
+
+if not exist darknet(
+  git clone https://github.com/AlexeyAB/darknet
+)
 cd darknet
+
 Powershell.exe -executionpolicy Bypass -File build.ps1 -UseVCPKG -EnableOPENCV -DisableInteractive
 pause
